f8d310a784d379ff3d976a1e16e46a7d40b41cee
Bethany Fannin <contact@bethanyfannin.com>
2025-12-14 23:22:42 -0500

    refactor: replace UnoCSS with my own CSS styles and utilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import UnoCSS from 'unocss/astro';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [
-        UnoCSS({
-            /* options */
-        }),
-        react(),
+        preact(),
     ]
 });
